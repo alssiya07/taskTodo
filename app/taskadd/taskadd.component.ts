185ea171be946dc6025b971fe5bcad0973bb6c9d
alssiya <allukukku2000@gmail.com>
2023-01-24 15:19:52 +0530

    jan 24

--- app/taskadd/taskadd.component.ts
+++ app/taskadd/taskadd.component.ts
@@ -1,6 +1,6 @@
 import { outputAst } from '@angular/compiler';
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormGroup,FormControl,Validators } from "@angular/forms";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { TaskService } from '../serivces/task.service';
 @Component({
   selector: 'app-taskadd',
@@ -9,21 +9,23 @@ import { TaskService } from '../serivces/task.service';
 })
 export class TaskaddComponent {
 
-  @Output()notify:EventEmitter<boolean>=new EventEmitter<boolean>()
+  @Output() notify: EventEmitter<boolean> = new EventEmitter<boolean>()
 
-  constructor(private service:TaskService){}
-taskform= new FormGroup({
-  "task_name": new FormControl("", [Validators.required])
-})
-get task_name() {
-  return this.taskform.get("task_name")
-}
-createtask() {
-  let data = this.taskform.value
-  this.service.addTask(data)
-  .then((res: any) => res.json())
-  .then(data => console.log("task created"))
-  .catch(err => alert(err))
-  this.notify.emit(true)
-}
+  constructor(private service: TaskService) { }
+  taskform = new FormGroup({
+    "task_name": new FormControl("", [Validators.required])
+  })
+  get task_name() {
+    return this.taskform.get("task_name")
+  }
+  createtask() {
+    this.notify.emit(false)
+    let data = this.taskform.value
+    this.service.addTask(data)
+      .then((res: any) => res.json())
+      .then(data => {
+        console.log("task created")
+        this.notify.emit(true)
+      }).catch(err => alert(err))
+  }
 }
--- app/tasklist/tasklist.component.ts
+++ app/tasklist/tasklist.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, DoCheck } from '@angular/core';
+import { Component, Input, OnInit, DoCheck,OnChanges, SimpleChanges } from '@angular/core';
 import { TaskService } from '../serivces/task.service';
 
 
@@ -7,7 +7,7 @@ import { TaskService } from '../serivces/task.service';
   templateUrl: './tasklist.component.html',
   styleUrls: ['./tasklist.component.css']
 })
-export class TasklistComponent implements OnInit{
+export class TasklistComponent implements OnInit,OnChanges{
 constructor(private service:TaskService){}
 @Input()inpt:boolean=false
 allTask:any
@@ -15,18 +15,18 @@ allTask:any
   ngOnInit():void{
     this.service.listTask().then((res: any) => res.json()).then(data => this.allTask(data)).catch(err => alert("cannot list"))
   }
-  // ngOnChanges(changes: SimpleChanges): void {
-  //   console.log(this.inpt);
-  //   console.log("invoke",this.inpt);
-  //   if (this.inpt){
-  //   this.service.listTask().then((res: any) => res.json()).then(data => this.allTask(data)).catch(err => alert("cannot list"))
-  //   }
-  // }
+  ngOnChanges(): void {
+    console.log(this.inpt);
+    
+    if (this.inpt){
+    this.service.listTask().then((res: any) => res.json()).then(data => this.allTask(data)).catch(err => alert("cannot list"))
+    }
+  }
   // ngDoCheck(): void {
   //   this.service.listTask().then((res: any) => res.json()).then(data => this.allTask(data))
     
   // }
   deleteTask(id:number){
-    this.service.deleteTask(id).then((res: any) => res.json()).then(data=>console.log(data))
+    this.service.deleteTask(id).then((res: any) => this.ngOnChanges())
   }
 }
